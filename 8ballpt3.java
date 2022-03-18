fe55fa7fc4a6a3dc729031f8a2a86d01d7486a32
Ewan Munro <Ewan.Munro@kpmg.co.uk>
2022-03-18 10:08:54 +0000

    added responses

--- 8ballpt3.java
+++ 8ballpt3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class Magic8Ball {
+public class 8ballpt3 {
     public static void main (String[] args) {
          
         Scanner input = new Scanner(System.in);
@@ -8,7 +8,31 @@ public class Magic8Ball {
         System.out.println("What is your question?");
         String name = input.nextLine();
                  
-        String[] wordList = {"Yes","Maybe","No"};
+        String[] wordList = {"Yes",
+        "Maybe",
+        "No", 
+        "As I see it, yes",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don’t count on it",
+        "It is certain",
+        "It is decidedly so",
+        "Most likely",
+        "My reply is no",
+        "My sources say no",
+        "Outlook good",
+        "Outlook not so good",
+        "Reply hazy try again",
+        "Signs point to yes",
+        "Very doubtful",
+        "Without a doubt",
+        "Yes, definitely",
+        "You may rely on it",
+        "You have to be kidding",
+        "You didn't seriously just ask that",
+        "I think its better I don't answer that"};
         int oneLength = wordList.length;
         int rand1 = (int) (Math.random() * oneLength);
         String phrase = wordList[rand1];
